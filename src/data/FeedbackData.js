387676691e92900998dea0e14007c90112095d09
Braulin Pires <100023268+Braulin96@users.noreply.github.com>
2022-10-30 19:12:06 +0000

    end of module 3

---
+++ src/data/FeedbackData.js
@@ -0,0 +1,23 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 10,
+        text: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio sed sit asperiores, temporibus eaque aspernatur, molestiae voluptatibus, recusandae perferendis nihil dolore blanditiis rem commodi maxime. Quisquam explicabo ducimus iste dolore.',
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio sed sit asperiores, temporibus eaque aspernatur, molestiae voluptatibus, recusandae perferendis nihil dolore blanditiis rem commodi maxime. Quisquam explicabo ducimus iste dolore.',
+    },
+    {
+        id: 3,
+        rating: 8,
+        text: ' Lorem, ipsum dolor sit amet consectetur adipisicing elit. Optio sed sit asperiores, temporibus eaque aspernatur, molestiae voluptatibus, recusandae perferendis nihil dolore blanditiis rem commodi maxime. Quisquam explicabo ducimus iste dolore.',
+    }
+]
+ 
+export default FeedbackData
+
+
+
+
